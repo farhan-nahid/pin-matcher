084dff11c608c32c12aff5e74b0a924e4b4fdb99
farhan-nahid <nahidahmed9933@gmail.com>
2021-09-03 18:29:57 +0600

    design done

--- app.js
+++ app.js
@@ -1,48 +1,53 @@
-function getPin(){
-    const random = Math.random()*10000;
-    const pin =( random +"").split('.')[0];
-    if(pin.length === 4){
-        return pin ;
-    }else{
-        return getPin();
-    }
+/*
+ ==========================================================================
+function getPin() {
+  const random = Math.random() * 10000;
+  const pin = (random + "").split(".")[0];
+  if (pin.length === 4) {
+    return pin;
+  } else {
+    return getPin();
+  }
 }
 
 // Display generated  pin
-function generatePin(){
-    const pinInput = document.getElementById('pin').value=getPin()
+function generatePin() {
+  document.getElementById("pin").value = getPin();
 }
 
 //handle calculator button event
-const buttonContainer = document.getElementById("button-container").addEventListener('click', function (event){
-   const digit= event.target.innerText;
+const buttonContainer = document
+  .getElementById("button-container")
+  .addEventListener("click", function (event) {
+    const digit = event.target.innerText;
 
-   if(isNaN(digit)){
-    if(digit === 'C'){
+    if (isNaN(digit)) {
+      if (digit === "C") {
         const typeInput = document.getElementById("type-pin");
-       typeInput.value='';
+        typeInput.value = "";
+      }
+    } else {
+      const typeInput = document.getElementById("type-pin");
+      typeInput.value = typeInput.value + digit;
     }
-   }  else{
-       const typeInput = document.getElementById("type-pin");
-       typeInput.value=typeInput.value+digit;
-   }
-})
+  });
 
 // verify pin
-function checkPin(){
-    const pin = document.getElementById('pin').value;
-    const typePin = document.getElementById("type-pin").value;
-    if(pin === typePin){
-        displayMatchResult('block', 'none');
-    }else{
-        displayMatchResult('none', 'block');
-    }
+function checkPin() {
+  const pin = document.getElementById("pin").value;
+  const typePin = document.getElementById("type-pin").value;
+  if (pin === typePin) {
+    displayMatchResult("block", "none");
+  } else {
+    displayMatchResult("none", "block");
+  }
 }
 
-function displayMatchResult(correctStatus, incorrectStatus){
-    const correct =  document.getElementById("correct");
-        correct.style.display = correctStatus;
-        const incorrect =  document.getElementById("incorrect");
-        incorrect.style.display = incorrectStatus;
+function displayMatchResult(correctStatus, incorrectStatus) {
+  const correct = document.getElementById("correct");
+  correct.style.display = correctStatus;
+  const incorrect = document.getElementById("incorrect");
+  incorrect.style.display = incorrectStatus;
 }
-
+ ==========================================================================
+ */
